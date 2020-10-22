7c28392db44afe7c090111eea2872d2d0b6f9a40
Вика <vikasilver77@mail.ru>
2020-10-22 09:28:14 +0300

    latest version

--- src/js/script.js
+++ src/js/script.js
@@ -114,5 +114,12 @@ $(document).ready(function(){
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
+
+    $("a[href=#catalog]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
     new WOW().init();
 });
